c501c800bb04921224cd2cad47f09cd1ab229053
Vadym Stukalov <vadym7@ukr.net>
2024-03-13 02:19:07 +0200

    adaptive finished, progect finished

--- app/js/main.js
+++ app/js/main.js
@@ -24,7 +24,40 @@ $(function () {
     slidesToScroll: 10,
     dots: true,
     arrows: false,
+    responsive: [
+      {
+        breakpoint: 1700,
+        settings: {
+          slidesToShow: 8,
+          slidesToScroll: 8,
+        },
+      },
+      {
+        breakpoint: 1511,
+        settings: {
+          slidesToShow: 6,
+          slidesToScroll: 6,
+        },
+      },
+      {
+        breakpoint: 1201,
+        settings: { slidesToShow: 4, slidesToScroll: 4 },
+      },
+      {
+        breakpoint: 841,
+        settings: { slidesToShow: 3, slidesToScroll: 3 },
+      },
+      {
+        breakpoint: 551,
+        settings: { slidesToShow: 2, slidesToScroll: 2 },
+      },
+      {
+        breakpoint: 376,
+        settings: { slidesToShow: 1, slidesToScroll: 1 },
+      },
+    ],
   });
+
   $(".article-slider__box").slick({
     prevArrow:
       '<button type="button" class="article-slider__arrow article-slider__arrow-left"> <img src="images/arrow-slide-left.svg" alt="img" />  </button>',
